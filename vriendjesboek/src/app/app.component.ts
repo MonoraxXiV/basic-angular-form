4f17adf046f5b16a0cb8ebaf145e40820b431615
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-10 11:09:30 +0100

    Finished step 10

---
+++ vriendjesboek/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'vriendjesboek';
+  languages = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby'];
+}
