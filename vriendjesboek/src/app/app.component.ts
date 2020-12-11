c10e9e03b9c9216e1124c34edbfa5dbf68dbd46b
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-11 11:26:53 +0100

    finished step 14, added httpmodules and add friend service

--- vriendjesboek/src/app/app.component.ts
+++ vriendjesboek/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Friend} from './friend';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,4 +11,9 @@ export class AppComponent {
   title = 'vriendjesboek';
   languages = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby'];
   friendModel = new Friend(null,null,null,null,null);
+  onSubmit(): void {
+    console.log(this.friendModel);
+  }
+
 }
+
