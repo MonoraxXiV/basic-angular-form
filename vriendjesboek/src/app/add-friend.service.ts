1860f87b2aeb00557a05df5589eac73d6d59ce7e
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-11 12:28:18 +0100

    finished step 15, http requests, added comments in components about possible edit

--- vriendjesboek/src/app/add-friend.service.ts
+++ vriendjesboek/src/app/add-friend.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Friend} from './friend';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddFriendService {
-private http: HttpClient;
+  private http: HttpClient;
 
   constructor(http: HttpClient) {
     this.http = http;
+  }
+
+  public addFriend(): Friend {
+    const url = '';
+    return this.http.post(url, data);
   }
 }
--- vriendjesboek/src/app/app.component.ts
+++ vriendjesboek/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {Friend} from './friend';
+import {AddFriendService} from './add-friend.service';
 
 
 @Component({
@@ -8,10 +10,19 @@ import {Friend} from './friend';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private addFriendService: AddFriendService;
+
   title = 'vriendjesboek';
   languages = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Ruby'];
-  friendModel = new Friend(null,null,null,null,null);
+  friendModel = new Friend(null, null, null, null, null);
+
+  constructor(addFriendService: AddFriendService) {
+    this.addFriendService = addFriendService;
+  }
   onSubmit(): void {
+    const observable = this.addFriendService.addFriend();
+    // added quotation marks to "it worked", might need to remove those later
+    observable.subscribe(data => "it worked", error => "it didn't work").
     console.log(this.friendModel);
   }
 
